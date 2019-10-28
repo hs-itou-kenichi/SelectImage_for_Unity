04d23fed6bd018f3505c8ada86cf80b36960afba
ItouKenichi <itou@heartsoftnoMacBook-puro-5.local>
2019-10-28 17:20:01 +0900

    init commit

---
+++ Assets/TapStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TapStart : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Invoke("ChangeScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
